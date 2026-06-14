3155dfe0ac420361a65f0a2d7edb1067b5712a99
rjakeeer <alexei_evdokimov@yahoo.com>
2026-06-14 06:12:01 +0500

    Update settings.py
    added server host to allowed

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0=ux4iaq&cz!@d-op=o&0!$-$4dh(d2_m6+z6ep6_jyly18i*c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['111.88.159.249', 'localhost', '127.0.0.1', '*']
 
 
 # Application definition
